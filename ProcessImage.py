b20a51198be6f7029ad81599b50e196f5cca5874
Leonardo <leonardoalvesbezerra@gmail.com>
2025-05-14 22:02:01 -0300

    Update comments

--- ProcessImage.py
+++ ProcessImage.py
@@ -13,28 +13,28 @@ class Enhance:
         self.resize()
         return self.resized
 
-        # Aplicar o filtro de Canny para detectar os arestas
+        # Aplly Canny edge filter (currently unused)
     def canny_edge(self):
         self.canny = cv2.Canny(self.img, 100, 200, apertureSize=3, L2gradient=True)
 
-        # Aplica desfoque Gaussiano para reduzir ruído
+        # Apply gaussian blur to reduce noise
     def gaussian_blur(self):
         self.gaussian = cv2.GaussianBlur(self.img, (3, 3), 0)
 
-        # Tornar imagem cinza (para facilitar leitura)
+        # Gray scale image to facilitate reading
     def gray_scale(self):
         self.gray_scale_img = cv2.cvtColor(self.gaussian, cv2.COLOR_BGR2GRAY)
 
-        # Aplicar thresholding pra aumentar o contraste do texto em preto e branco de forma otimizada
+        # Apply thresholding to enhance optimized contrast in black and white
     def thresholding(self):
         _, self.thresh = cv2.threshold(self.gray_scale_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # Aumentar a imagem para deixar o texto mais facil de ler
+        # Reescale the image to make it easier to read
     def resize(self):
         self.resized = cv2.resize(self.thresh, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
 
-        # Abrir janelas com as etapas de tratamento de imagem desejadas
+        # Pop up windows with desired steps (used for development only)
     def show_steps(self):
         cv2.namedWindow("Thresholding", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("Thresholding", 800, 600)
@@ -44,7 +44,7 @@ class Enhance:
         cv2.resizeWindow("Canny Edge", 800, 600)
         cv2.imshow("Canny Edge", self.canny)
 
-        cv2.waitKey(0)
+        cv2.waitKey(0)  # Wait key press to close windows
         cv2.destroyAllWindows()
 
 
--- main.py
+++ main.py
@@ -3,29 +3,31 @@ import numpy as np
 import cv2
 import pytesseract
 import ProcessImage as process
+# pip install fastapi uvicorn python-multipart numpy opencv-python pytesseract
+# also download Tesseract via web
 
 app = FastAPI()
 
-  # Rota para rodar o ocr
+  # Endpoint to run OCR receiving target image as POST method
 @app.post("/run-ocr/")
 async def run_ocr(file: UploadFile = File(...)):
     try:
-        # Ler arquivo
+        # Read file
       file_contents = await file.read()
 
-        # Converter para NumPy array e depois para formato cv2
+        # Convert to NumPy array then cv2 format
       np_array = np.frombuffer(file_contents, np.uint8)
       image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
       
       if image is not None:
-        # process_image = process.Enhance(image["path"])
-        process_image = process.Enhance(image)
-        processed_image = process_image.execute()
+        process_image = process.Enhance(image)  # Initialize class with target image
+        processed_image = process_image.execute()  # Run execute function
 
+          # Extract and return OCR result
         extracted_text = pytesseract.image_to_string(processed_image, lang='por')
         print(extracted_text)
 
-        process_image.show_steps()
+        process_image.show_steps()  # Show desired steps from image enhancing process
         return extracted_text
       else: 
         return {"error": "Image not found"}
